80d540da055be4b80ef7203efe4f2b1e7f26d7ec
kelvin <kelvininfortec@gmail.com>
2024-02-20 19:00:13 -0300

    chore: add model user

---
+++ src/external/models/users.py
@@ -0,0 +1,26 @@
+from src.external.infra.sql.sqlalchemy import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer
+
+class Users(Base):
+    __tablename__ = "users"
+
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    first_name: Mapped[str]  = mapped_column(String, nullable=False)
+    last_name: Mapped[str] = mapped_column(String, nullable=False)
+    age: Mapped[int] = mapped_column(Integer, nullable=False)
+
+
+    def __repr__(self):
+        return f"Users (id={self.id}, first_name={self.first_name})"
+    
+
+    @property
+    def to_json(self):
+        return {
+            "id": str(self.id),
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+        }
